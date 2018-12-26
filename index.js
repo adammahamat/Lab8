bdb8599b97a5735d5885dcda4bcaea1915b0e24b
adammahamat <adammahamat493@gmail.com>
2018-12-26 20:20:07 +0300

    arranged to a # files

---
+++ index.js
@@ -0,0 +1,54 @@
+var types = {
+    plus: true,
+    minus: true,
+    linse: true,
+    glass: true,
+};
+
+function filter() {
+    var root = document.getElementById('content');
+
+    var nodes = root.childNodes;
+
+    for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].nodeName != "#text") {
+            var className = nodes[i].className;
+            var split = className.split(' ');
+
+            var type = split[1];
+            var dist = split[2];
+
+            var can = true;
+
+            if (type == "linse" && !types.linse)
+                can = false;
+
+            if (type == "glass" && !types.glass)
+                can = false;
+
+            if (dist == "+" && !types.plus)
+                can = false;
+
+            if (dist == "-" && !types.minus)
+                can = false;
+
+            if (can)
+                nodes[i].style.display = "inline-block";
+            else
+                nodes[i].style.display = "none";
+        }
+    }
+}
+
+function clicked(value) {
+    var el = document.getElementById(value);
+
+    types[value] = !types[value];
+
+    if (types[value])
+        el.className = "type selected";
+    else
+        el.className = "type";
+
+    filter();
+}
